654b39244b8bdac09e2fd8813a3bfe3fe33eae24
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2020-07-14 15:47:58 -0700

    Remove row and column names from csv, remove unnecessary code

--- model data transformation/occupancy_analysis.R
+++ model data transformation/occupancy_analysis.R
@@ -15,18 +15,6 @@ camop <- cameraOperation(CTtable      = camtraps,
                          dateFormat   = "mdy"
 )
 
-#unnecessary because record table already exists
-# define image directory
-#wd_images_ID <- system.file("pictures/sample_images_species_dir", package = "camtrapR")
-
-# make record table
-#recordTableSample <- recordTable(inDir               = wd_images_ID,
-#                                 IDfrom              = "directory",
-#                                 minDeltaTime        = 15,
-#                                 deltaTimeComparedTo = "lastIndependentRecord",
-#                                 timeZone            = "Asia/Kuala_Lumpur"
-#)
-
 #load in Gorongosa record table
 record_table <- read_csv("data/gorongosa-cameras/recordtable_allrecordscleaned_speciesmetadata.csv")
 
@@ -42,7 +30,9 @@ DetHist_genet <- detectionHistory(recordTable     = record_table,
                              occasionLength       = 1, #sampling period (in days) represented by a single column in the occupancy matrix
                              day1                 = "survey", #dates/columns in resulting matrix will match up (starts each row on the date the first camera was set up)
                              includeEffort        = FALSE,
-                             occasionStartTime    = 12,  #start at noon b/c nocturnal animals
-                             writecsv             = TRUE,
-                             outDir               = "model data transformation" 
+                             occasionStartTime    = 12  #start at noon b/c nocturnal animals
                              )
+
+DetHist_genet <- as.data.frame(DetHist_genet)
+
+write_csv(DetHist_genet, "data/gorongosa-cameras/genet.csv", col_names = F)
