efa6ee346773c03d07ee4697672cce873e040ed9
klg-2016 <kathrynl.grabowski@gmail.com>
2020-07-16 11:50:40 -0400

    added detection history for civets

--- model data transformation/occupancy_analysis.R
+++ model data transformation/occupancy_analysis.R
@@ -36,3 +36,22 @@ DetHist_genet <- detectionHistory(recordTable     = record_table,
 DetHist_genet <- as.data.frame(DetHist_genet)
 
 write_csv(DetHist_genet, "data/gorongosa-cameras/genet.csv", col_names = F)
+
+# make detection history for civets (without trapping effort)
+DetHist_civet <- detectionHistory(recordTable          = record_table,
+                                  camOp                = camop,
+                                  stationCol           = "Camera",
+                                  speciesCol           = "Species",
+                                  recordDateTimeCol    = "DateTimeOriginal",
+                                  recordDateTimeFormat = "%m/%d/%y %H:%M",
+                                  timeZone             = "Africa/Maputo",
+                                  species              = "Civet",
+                                  occasionLength       = 1, #sampling period (in days) represented by a single column in the occupancy matrix
+                                  day1                 = "survey", #dates/columns in resulting matrix will match up (starts each row on the date the first camera was set up)
+                                  includeEffort        = FALSE,
+                                  occasionStartTime    = 12  #start at noon b/c nocturnal animals
+)
+
+DetHist_civet <- as.data.frame(DetHist_civet)
+
+write_csv(DetHist_civet, "data/gorongosa-cameras/civet.csv", col_names = F)
