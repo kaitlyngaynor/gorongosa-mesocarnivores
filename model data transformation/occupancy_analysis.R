9b47bcbefc3b027180c50d5a306286b3fb0c70b4
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2020-07-16 09:30:10 -0700

    Subset to start and end dates of interest

--- model data transformation/occupancy_analysis.R
+++ model data transformation/occupancy_analysis.R
@@ -3,6 +3,10 @@
 library(camtrapR) #install.packages("camtrapR")
 library(tidyverse)
 
+# define start and end date - these are used to subset both operation matrix and record table
+start.date <- "2016-07-01"
+end.date <- "2016-09-30"
+
 #load in Gorongosa camera operations data table
 camtraps <- read_csv("data/gorongosa-cameras/Camera_operation_years1and2.csv")
 
@@ -15,12 +19,24 @@ camop <- cameraOperation(CTtable      = camtraps,
                          dateFormat   = "mdy"
 )
 
-#load in Gorongosa record table
+# subset to dates of interest
+camop_subset <- camop %>% 
+    as.data.frame %>% # first need to convert matrix to data frame
+    select(start.date:end.date) %>% # select columns of interest
+    as.matrix() # get it back into matrix form for calculating detection history
+
+# load in Gorongosa record table
 record_table <- read_csv("data/gorongosa-cameras/recordtable_allrecordscleaned_speciesmetadata.csv")
 
+# subset to dates of interest
+record_table_subset <- record_table %>% 
+    mutate(Date = as.Date(DateTimeOriginal, # format date column as date for subsetting
+                          format = "%m/%d/%y %H:%M")) %>% 
+    filter(Date >= as.Date(start.date) & Date <= as.Date(end.date))
+
 # make detection history for genets (without trapping effort)
-DetHist_genet <- detectionHistory(recordTable     = record_table,
-                             camOp                = camop,
+DetHist_genet <- detectionHistory(recordTable     = record_table_subset,
+                             camOp                = camop_subset,
                              stationCol           = "Camera",
                              speciesCol           = "Species",
                              recordDateTimeCol    = "DateTimeOriginal",
@@ -38,8 +54,8 @@ DetHist_genet <- as.data.frame(DetHist_genet)
 write_csv(DetHist_genet, "data/gorongosa-cameras/genet.csv", col_names = F)
 
 # make detection history for civets (without trapping effort)
-DetHist_civet <- detectionHistory(recordTable          = record_table,
-                                  camOp                = camop,
+DetHist_civet <- detectionHistory(recordTable          = record_table_subset,
+                                  camOp                = camop_subset,
                                   stationCol           = "Camera",
                                   speciesCol           = "Species",
                                   recordDateTimeCol    = "DateTimeOriginal",
