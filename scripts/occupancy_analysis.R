cef2cb3d62c8cb94cd2198128ab581e3cd94861c
klg-2016 <kathrynl.grabowski@gmail.com>
2020-07-17 13:12:48 -0400

    added marsh mongoose detection history

--- scripts/occupancy_analysis.R
+++ scripts/occupancy_analysis.R
@@ -93,6 +93,26 @@ DetHist_honeybadger <- as.data.frame(DetHist_honeybadger)
 
 write_csv(DetHist_honeybadger, "data/gorongosa-cameras/honey_badger.csv", col_names = F)
 
+
+# make detection history for marsh mongooses (without trapping effort)
+DetHist_marshmongoose <- detectionHistory(recordTable  = record_table_subset,
+                                  camOp                = camop_subset,
+                                  stationCol           = "Camera",
+                                  speciesCol           = "Species",
+                                  recordDateTimeCol    = "DateTimeOriginal",
+                                  recordDateTimeFormat = "%m/%d/%y %H:%M",
+                                  timeZone             = "Africa/Maputo",
+                                  species              = "Mongoose_marsh",
+                                  occasionLength       = 1, #sampling period (in days) represented by a single column in the occupancy matrix
+                                  day1                 = "survey", #dates/columns in resulting matrix will match up (starts each row on the date the first camera was set up)
+                                  includeEffort        = FALSE,
+                                  occasionStartTime    = 12  #start at noon b/c nocturnal animals
+)
+
+DetHist_marshmongoose <- as.data.frame(DetHist_marshmongoose)
+
+write_csv(DetHist_marshmongoose, "data/gorongosa-cameras/marsh_mongoose.csv", col_names = F)
+
 # make covariate tables
 
 # load in GNP cam metadata
