750c020178b6b68b1ba0ed4c17f866138a2b287b
klg-2016 <kathrynl.grabowski@gmail.com>
2020-07-17 12:07:56 -0400

    added termite mound variable to psi table; added honey badger detection history

--- scripts/occupancy_analysis.R
+++ scripts/occupancy_analysis.R
@@ -74,14 +74,33 @@ DetHist_civet <- as.data.frame(DetHist_civet)
 
 write_csv(DetHist_civet, "data/gorongosa-cameras/civet.csv", col_names = F)
 
+# make detection history for honey badgers (without trapping effort)
+DetHist_honeybadger <- detectionHistory(recordTable    = record_table_subset,
+                                  camOp                = camop_subset,
+                                  stationCol           = "Camera",
+                                  speciesCol           = "Species",
+                                  recordDateTimeCol    = "DateTimeOriginal",
+                                  recordDateTimeFormat = "%m/%d/%y %H:%M",
+                                  timeZone             = "Africa/Maputo",
+                                  species              = "Honey_badger",
+                                  occasionLength       = 1, #sampling period (in days) represented by a single column in the occupancy matrix
+                                  day1                 = "survey", #dates/columns in resulting matrix will match up (starts each row on the date the first camera was set up)
+                                  includeEffort        = FALSE,
+                                  occasionStartTime    = 12  #start at noon b/c nocturnal animals
+)
+
+DetHist_honeybadger <- as.data.frame(DetHist_honeybadger)
+
+write_csv(DetHist_honeybadger, "data/gorongosa-cameras/honey_badger.csv", col_names = F)
+
 # make covariate tables
 
-# load in cam metadata
+# load in GNP cam metadata
 cam_meta <- read_csv("data/gorongosa-cameras/cam_metadata_fromfield_and_raw_raster_withlion.csv")
 
 # make psi covariates table (environmental)
-# need to add termite and lion covariates when we decide
-GNP_psi_covariates <- select(cam_meta, urema_dist, tree_hansen)
+# need to add lion covariate when we decide
+GNP_psi_covariates <- select(cam_meta, urema_dist, tree_hansen, termite.large.count.100m)
 
 write_csv(GNP_psi_covariates, "data/gorongosa-cameras/GNP psi covariates.csv", col_names = T)
 
