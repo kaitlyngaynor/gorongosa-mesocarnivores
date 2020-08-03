2a1f835639ac0732eff1d6b9892c268548027d07
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2020-08-03 16:47:27 -0700

    Rename and reorder

---
+++ scripts/alternative-multispecies-model/04-species-covariate-plot.R
@@ -0,0 +1,101 @@
+library(tidyverse)
+library(here)
+library(patchwork)
+
+# PREP DATA
+
+# bring in group covariate means for model with groups
+sppcov <- read.csv(here::here("scripts/alternative-multispecies-model/results/mesocarnivore_species.csv"))
+
+# bring in species (for full common names and groups)
+Spp <- read.csv("~/Documents/github-repos/gorongosa/gorongosa-camera-traps/data/2018spp_kingdon.csv") # Species IDs and traits
+
+# make keys for changing factor levels and names (not sure if a better way...)
+factor_key <- data.frame(Levels_Factor = 1:3,
+                         Factor = c("urema.distance", "tree.hansen", "termite.count.100m"),
+                         Factor_Full = c("Lake Proximity", "Tree Cover", "Termite Mound Density"))
+
+data_to_plot <- sppcov %>% 
+    rename(SppCode = Species) %>% 
+    left_join(Spp) %>% 
+    left_join(factor_key) %>% 
+    mutate(Factor_Full = fct_reorder(Factor_Full, Levels_Factor)) 
+
+# make the distance values negative so that they can be 'proximity' values
+for(i in 1:nrow(data_to_plot)) {
+    if(data_to_plot[i,1] %in% c("urema.distance")) {
+        data_to_plot[i,3] <- data_to_plot[i,3] * -1
+        data_to_plot[i,5] <- data_to_plot[i,5] * -1
+        data_to_plot[i,6] <- data_to_plot[i,6] * -1
+    }
+}
+
+# PLOT
+
+# Make theme
+sppcov_theme <-     theme(axis.title.y = element_blank(),
+                     #     legend.title = element_blank(),
+                          legend.key = element_rect(fill = alpha("white", 0.0)),
+                          strip.background = element_blank(),
+                          strip.text = element_blank(),
+                          panel.grid.major = element_blank(),
+                          panel.background = element_blank(),
+                          axis.ticks.y = element_blank(),
+                          axis.text.y = element_text(size=rel(1.25)),
+                          panel.border = element_rect(color = "black", fill = NA, size = .5),
+                          legend.text=element_text(size=rel(1)))
+
+# Tree --------------------------------------------------------------------
+
+tree <-
+    data_to_plot %>% 
+    subset(Factor == "tree_hansen") %>% 
+    mutate(CommName_Full = fct_reorder(CommName_Full, Mean)) %>% 
+    ggplot(aes(x = CommName_Full, y = Mean)) + 
+    geom_hline(aes(yintercept = 0), linetype="dashed", size = 0.5, color = "darkgrey") +
+    geom_errorbar(aes(ymin = LCI, ymax = UCI), width=0, position = position_dodge(width = 1), alpha=.75) +
+    geom_point(position = position_dodge(width = 1), size = 2) +
+    sppcov_theme +
+    coord_flip() + # switch x and y coordinates
+    labs(y = "Beta Coefficient - Tree Density") 
+
+# Lake -------------------------------------------------------------------
+
+lake <-
+    data_to_plot %>% 
+    subset(Factor == "urema_dist") %>% 
+    mutate(CommName_Full = fct_reorder(CommName_Full, Mean)) %>% 
+    ggplot(aes(x = CommName_Full, y = Mean)) + 
+    geom_hline(aes(yintercept = 0), linetype="dashed", size = 0.5, color = "darkgrey") +
+    geom_errorbar(aes(ymin = LCI, ymax = UCI), width=0, position = position_dodge(width = 1), alpha=.75) +
+    geom_point(position = position_dodge(width = 1), size = 2) +
+    sppcov_theme +
+    coord_flip() + # switch x and y coordinates
+    labs(y = "Beta Coefficient - Lake Proximity") 
+    
+# Termite --------------------------------------------------------------------
+
+termite <-
+    data_to_plot %>% 
+    subset(Factor == "termite.count.100m") %>% 
+    mutate(CommName_Full = fct_reorder(CommName_Full, Mean)) %>% 
+    ggplot(aes(x = CommName_Full, y = Mean)) + 
+    geom_hline(aes(yintercept = 0), linetype="dashed", size = 0.5, color = "darkgrey") +
+    geom_errorbar(aes(ymin = LCI, ymax = UCI), width=0, position = position_dodge(width = 1), alpha=.75) +
+    geom_point(position = position_dodge(width = 1), size = 2) +
+    sppcov_theme +
+    coord_flip() + # switch x and y coordinates
+    labs(y = "Beta Coefficient - Termite Mound Density") 
+
+
+pdf("scripts/alternative-multispecies-model/figures//sppcov-termite.pdf", width = 6, height = 5)
+termite
+dev.off()
+
+pdf("scripts/alternative-multispecies-model/figures//sppcov-tree.pdf", width = 6, height = 5)
+tree
+dev.off()
+
+pdf("scripts/alternative-multispecies-model/figures//sppcov-lake.pdf", width = 6, height = 5)
+lake
+dev.off()
---
+++ scripts/alternative-multispecies-model/05-richness-covariate-plots.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+library(here)
+library(ggpubr)
+
+# read in model output - modeled species richness at each camera trap site
+site.richness <- read.csv(here::here("scripts/alternative-multispecies-model/results/mesocarnivore_spprich.csv"))
+
+# bring in associated site metadata
+site.metadata <- read.csv("data/gorongosa-cameras/cam_metadata_fromfield_and_raw_raster.csv")
+
+# merge modeled species richness and site metadata
+site.richness <- left_join(site.richness, site.metadata)
+
+# plot species richness vs termite mound count
+termite <- ggplot(site.richness, aes(x = termite.count.100m, y = Mean)) + 
+    geom_errorbar(aes(ymin=LCI, ymax=UCI), width=0, colour = "lightgrey") +
+    geom_point() + 
+    geom_smooth(method = lm, se = F, col = "black", linetype = "dashed", size = 0.5) +
+    xlab("Termite Mound Density (mounds w/in 100m)") +
+    ylab("Mesocarnivore Richness") + 
+    theme_bw() 
+
+# plot species richness vs road distance
+road <- ggplot(site.richness, aes(x = road_major_dist/1000, y = Mean)) + 
+    geom_errorbar(aes(ymin=LCI, ymax=UCI), width=0, colour = "lightgrey") +
+    geom_point() + 
+    geom_smooth(method = lm, se = F, col = "black", linetype = "dashed", size = 0.5) +
+    xlab("Proximity to Road (km)") +
+    ylab("Mesocarnivore Richness") + 
+    theme_bw() +
+    scale_x_reverse() 
+
+# plot species richness vs Urema distance
+lake <- ggplot(site.richness, aes(x = urema_dist/1000, y = Mean)) + 
+    geom_errorbar(aes(ymin=LCI, ymax=UCI), width=0, colour = "lightgrey") +
+    geom_point() + 
+    geom_smooth(method = lm, se = F, col = "black", linetype = "dashed", size = 0.5) +
+    xlab("Proximity to Lake Urema (km)") +
+    ylab("Mesocarnivore Richness") + 
+    theme_bw() +
+    scale_x_reverse() 
+
+# plot species richness vs tree cover
+tree <- ggplot(site.richness, aes(x = tree_hansen, y = Mean)) +
+    geom_errorbar(aes(ymin=LCI, ymax=UCI), width=0, colour = "lightgrey") +
+    geom_point() + 
+    geom_smooth(method = lm, se = F, col = "black", linetype = "dashed", size = 0.5) +
+    xlab("Tree Cover (%)") +
+    ylab("Mesocarnivore Richness") + 
+    theme_bw()
+
+pdf("scripts/alternative-multispecies-model/figures/richness-covariates.pdf", width = 10, height = 6, useDingbats = FALSE)
+ggarrange(tree, lake, termite, road)
+dev.off()
