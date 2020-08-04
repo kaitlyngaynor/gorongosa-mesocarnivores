b0432d30e2a9f9e85cccfbf7e4b0e71fa83bad4b
klg-2016 <kathrynl.grabowski@gmail.com>
2020-08-04 12:21:37 -0400

    exploring single species models

---
+++ scripts/honey badger exploration.R
@@ -0,0 +1,37 @@
+library("unmarked") #install.packages("unmarked")
+
+# load in detection history
+honeybadger_dh <-read_csv("data/gorongosa-cameras/honey_badger.csv", col_names = FALSE) %>% as.matrix()
+
+#load occupancy covariates
+#leaving the column names in for now
+occ_covs <- read_csv("data/gorongosa-cameras/GNP covariates.csv", col_names = TRUE) %>% as.data.frame()
+
+#scaling all the non-binary covariates to address the NaN warnings
+occ_covs$urema_dist = scale(occ_covs$urema_dist)
+occ_covs$tree_hansen = scale(occ_covs$tree_hansen)
+occ_covs$termite.large.count.100m = scale(occ_covs$termite.large.count.100m)
+occ_covs$lion_latedry = scale(occ_covs$lion_latedry)
+occ_covs$cover.ground = scale(occ_covs$cover.ground)
+
+honeybadger_data <- unmarkedFrameOccu(honeybadger_dh, siteCovs = occ_covs)
+
+(honeybadger_fit1 <- occu(~cover.ground+detect.obscured ~urema_dist, honeybadger_data))
+
+(honeybadger_fit2 <- occu(~cover.ground+detect.obscured ~termite.large.count.100m, honeybadger_data))
+
+(honeybadger_fit3 <- occu(~cover.ground+detect.obscured ~tree_hansen, honeybadger_data))
+
+(honeybadger_fit4 <- occu(~cover.ground+detect.obscured ~lion_latedry, honeybadger_data))
+
+(honeybadger_fit5 <- occu(~cover.ground+detect.obscured ~urema_dist+termite.large.count.100m, honeybadger_data))
+
+(honeybadger_fit6 <- occu(~cover.ground+detect.obscured ~urema_dist+tree_hansen, honeybadger_data))
+
+(honeybadger_fit7 <- occu(~cover.ground+detect.obscured ~urema_dist+lion_latedry, honeybadger_data))
+
+(honeybadger_fit8 <- occu(~cover.ground+detect.obscured ~termite.large.count.100m+tree_hansen, honeybadger_data))
+
+(honeybadger_fit9 <- occu(~cover.ground+detect.obscured ~termite.large.count.100m+lion_latedry, honeybadger_data))
+
+(honeybadger_fit10 <- occu(~cover.ground+detect.obscured ~tree_hansen+lion_latedry, honeybadger_data))
---
+++ scripts/marsh mongoose exploration.R
@@ -0,0 +1,37 @@
+library("unmarked") #install.packages("unmarked")
+
+# load in detection history
+marshmongoose_dh <-read_csv("data/gorongosa-cameras/marsh_mongoose.csv", col_names = FALSE) %>% as.matrix()
+
+#load occupancy covariates
+#leaving the column names in for now
+occ_covs <- read_csv("data/gorongosa-cameras/GNP covariates.csv", col_names = TRUE) %>% as.data.frame()
+
+#scaling all the non-binary covariates to address the NaN warnings
+occ_covs$urema_dist = scale(occ_covs$urema_dist)
+occ_covs$tree_hansen = scale(occ_covs$tree_hansen)
+occ_covs$termite.large.count.100m = scale(occ_covs$termite.large.count.100m)
+occ_covs$lion_latedry = scale(occ_covs$lion_latedry)
+occ_covs$cover.ground = scale(occ_covs$cover.ground)
+
+marshmongoose_data <- unmarkedFrameOccu(marshmongoose_dh, siteCovs = occ_covs)
+
+(marshmongoose_fit1 <- occu(~cover.ground+detect.obscured ~urema_dist, marshmongoose_data))
+
+(marshmongoose_fit2 <- occu(~cover.ground+detect.obscured ~termite.large.count.100m, marshmongoose_data))
+
+(marshmongoose_fit3 <- occu(~cover.ground+detect.obscured ~tree_hansen, marshmongoose_data))
+
+(marshmongoose_fit4 <- occu(~cover.ground+detect.obscured ~lion_latedry, marshmongoose_data))
+
+(marshmongoose_fit5 <- occu(~cover.ground+detect.obscured ~urema_dist+termite.large.count.100m, marshmongoose_data))
+
+(marshmongoose_fit6 <- occu(~cover.ground+detect.obscured ~urema_dist+tree_hansen, marshmongoose_data))
+
+(marshmongoose_fit7 <- occu(~cover.ground+detect.obscured ~urema_dist+lion_latedry, marshmongoose_data))
+
+(marshmongoose_fit8 <- occu(~cover.ground+detect.obscured ~termite.large.count.100m+tree_hansen, marshmongoose_data))
+
+(marshmongoose_fit9 <- occu(~cover.ground+detect.obscured ~termite.large.count.100m+lion_latedry, marshmongoose_data))
+
+(marshmongoose_fit10 <- occu(~cover.ground+detect.obscured ~tree_hansen+lion_latedry, marshmongoose_data))
