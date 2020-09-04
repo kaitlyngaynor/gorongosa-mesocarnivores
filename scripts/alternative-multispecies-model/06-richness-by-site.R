d9bf97410b1fb654e65edafad906f2fd97ea9e4d
klg-2016 <kathrynl.grabowski@gmail.com>
2020-09-04 00:15:16 -0400

    trying to count actual detections of each species

--- scripts/alternative-multispecies-model/06-richness-by-site.R
+++ scripts/alternative-multispecies-model/06-richness-by-site.R
@@ -7,7 +7,10 @@ library(stringr)
 library(here)
 library(paletteer)
 library("expss") #install.packages("expss")
-#install.packages("tibble")
+library("lubridate") #install.packages("lubridate")
+library(magrittr) #install.packages("magrittr")
+library(readr) #install.packages("readr")
+library(tibble) #install.packages("tibble")
 
 #bring in detection data for study period
 detection_dry <- read.csv("scripts/alternative-multispecies-model/detections-dry2016_katie-8_1_16-11_30_16.csv")
@@ -34,3 +37,18 @@ total_detections_by_species <- colSums(meso_detection_dry)
 #I can't figure out how to get this to add to my other data frame...
 # trans_meso_detection_dry <- trans_meso_detection_dry %>% add_column(total_detections)
 
+##ABOVE DOESN'T CALCULATE WHAT I THOUGHT I WAS CALCULATING, TRYING AGAIN
+
+record <- read_csv("data/gorongosa-cameras/recordtable_allrecordscleaned_speciesmetadata.csv")
+#Date = col_datetime(format = "")
+#  Complete date plus hours, minutes and seconds:
+# YYYY-MM-DDThh:mm:ssTZD (eg 1997-07-16T19:20:30+01:00)
+
+record_filtered <- record %>% select(Species, Date)  %>% #filter for species and date columns
+  filter(Date >= as.Date("2016-08-01T00:00:00+02:00") & Date <= as.Date("2016-11-30T23:59:59+02:00")) %>% #filter for late dry dates (Beira time)
+  dplyr::count(Species)
+
+#source("scripts/alternative-multispecies-model/01-generate-model-input.R")
+
+# Run for dry season that matches Katie
+# clean_for_occupancy(matrix.all = record, "8/1/16 00:00", "11/30/16 23:59", "dry2016_KLG", min.operation = 0)
